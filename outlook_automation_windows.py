731ebd91ebbeaa31af821f99700f9e56d985c0de
Md. Ariful Islam <arifultusharr@gmail.com>
2022-09-13 19:09:06 +0600

    updated: message printing for all steps

--- outlook_automation_windows.py
+++ outlook_automation_windows.py
@@ -5,7 +5,9 @@ from pathlib import Path
 
 # Remove backslash
 def modify_path_name(path_name): 
-    return path_name.replace('\\',r"/")
+    mod_path_name = path_name.replace('\\',r"/")
+    print("The path name is modified successfully!")
+    return mod_path_name
 
 # Create Desired Directory 
 def create_folder(PATH):
@@ -16,21 +18,28 @@ def create_folder(PATH):
 def download_attachments(path_name,sender_name,date_today,given_email):
 
     outlook = client.Dispatch("Outlook.Application")
+    print("Called client dispatch")
 
     mapi = outlook.GetNamespace("MAPI")
+    print("Got the Mapi object")
 
     # Check Username 
     for account in mapi.Accounts:
         email = account.DeliveryStore.DisplayName
 
         if email == given_email:
+            print("Email Matched with given email!")
 
             sender = sender_name.split("@")[0]
             path_original_name = f"{path_name}/{sender}/{date_today}"
             create_folder(path_original_name)
+            print("Created folder with given path name")
 
+            print("Trying to download the attachments")
             inbox = mapi.Folders(given_email).Folders("Inbox")
+            print("Got inbox object for given email")
             messages = inbox.Items 
+            print("Got messages in the folder")
             try:
                 for message in list(messages):
 
@@ -45,13 +54,12 @@ def download_attachments(path_name,sender_name,date_today,given_email):
             except Exception as e:
                 print("Error when processing emails messages:" + str(e))
 
-
-
 def main(): 
     email = "Please enter email here"
     path_name = r"Path name here"
     sender_name = "Sender email here"
     date_today =  datetime.today().strftime('%Y-%m-%d')
+    print(f"Todays Date: {date_today}")
     path_name_modified = modify_path_name(path_name)
     download_attachments(path_name_modified,sender_name,date_today,email)
 main()
