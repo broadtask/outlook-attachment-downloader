de2eaf813b2734dab77167b9a36a4f838dc7873e
Md. Ariful Islam <arifultusharr@gmail.com>
2022-10-07 18:31:37 +0600

    pylint improved

--- outlook_automation_windows_customized.py
+++ outlook_automation_windows_customized.py
@@ -1,187 +1,187 @@
+"""Import modules"""
 import sys
-import win32com.client as client 
-from datetime import datetime, timedelta
+from datetime import datetime
 import os
 from os import path
-import csv
-import time
 from pathlib import Path
 import pandas as pd
+from win32com import client
 MAIN_LIST = []
 
 
-def df_to_excel_main_list(data_list,date_and_time):
-    filename = modify_date_time(date_and_time).replace(":","_")
+def df_to_excel_main_list(data_list, date_and_time):
+    """Save data to excel"""
+    filename = modify_date_time(date_and_time).replace(":", "_")
     my_python_list = data_list
-    df = pd.DataFrame(columns=['Date Time','Folder Names','Message Count','Attachment Count'], data=my_python_list)
-    # df = pd.read_csv (f"{filename}.csv")
-    writer = pd.ExcelWriter(f'log_{filename}.xlsx', engine='xlsxwriter')
-    df.to_excel(writer, sheet_name='Sheet1',index = None)
-    workbook  = writer.book
-    worksheet = writer.sheets['Sheet1']
-    wraping = workbook.add_format({'text_wrap': True})
-    writer.save()
+    d_f = pd.DataFrame(columns=['Date Time',
+                                'Folder Names',
+                                'Message Count',
+                                'Attachment Count'], data=my_python_list)
+    # pylint: disable=E0110:
+    with pd.ExcelWriter(f'log_{filename}.xlsx', engine='xlsxwriter') as writer:
+        d_f.to_excel(writer, sheet_name='Sheet1', index=False)
 
 
-def save_csv(data_list,date_and_time): 
-
-    date_time_text = modify_date_time(date_and_time).replace(":","_")
-    print(date_time_text)
-    with open(f'./log_{date_time_text}.csv', "a", newline='', encoding='utf-8-sig') as fp:
-        wr = csv.writer(fp, dialect='excel')
-        wr.writerow(data_list)
-
-
-
-# Remove backslash
-def modify_path_name(path_name): 
-    mod_path_name = path_name.replace('\\',r"/")
+def modify_path_name(path_name):
+    """Remove backslash"""
+    mod_path_name = path_name.replace('\\', r"/")
     print("The path name is modified successfully!")
     return mod_path_name
 
-def modify_date_time(date_and_time): 
+
+def modify_date_time(date_and_time):
+    """Modify date time to text and remove the seconds"""
     date_time_text_split = f"{date_and_time}".split(":")
-    date_time_text_split.pop() 
+    date_time_text_split.pop()
     date_and_time_text = ":".join(date_time_text_split)
     return date_and_time_text
 
 
-# Create Desired Directory 
-def create_folder(PATH):
-    Path(PATH).mkdir(parents=True, exist_ok=True)
+def create_folder(path_name):
+    """Create desired directory"""
+    Path(path_name).mkdir(parents=True, exist_ok=True)
 
 
-def check_similar_file(filename,pathname):
-    c = 1
+def check_similar_file(filename, pathname):
+    """Check similar file in the given directory"""
+    count_data = 1
     new_filename = filename
     while True:
-        
-        if path.exists(pathname+'\\'+ new_filename): 
-            new_filename = f"{filename} ({c})"
-            c = c+1
-            continue
-        else: 
-            break 
-    return new_filename
-            
 
-def move_message(folders_object_data,date_and_time,message):
+        if path.exists(pathname+'\\' + new_filename):
+            new_filename = f"{filename} ({count_data})"
+            count_data = count_data+1
+            continue
+
+        return new_filename
+
+
+def move_message(folders_object_data, date_and_time, message):
+    """Move messages to different folder"""
     date_and_time_text = modify_date_time(date_and_time)
-    
-    try: 
+
+    try:
         folders_object_data[date_and_time_text]
-    except: 
+    except:  # pylint: disable=W0702
         folders_object_data.Add(date_and_time_text)
 
     message.Move(folders_object_data[date_and_time_text])
 
 
+# pylint: disable=R1702
 
-
-# Download Attachments 
-def download_attachments(path_name,date_today,status,date_and_time):
-
-
-
-
+def download_attachments(path_name, date_today, status, date_and_time):  # pylint: disable=R0914
+    # pylint: disable=R0915
+    """Attachment downloading"""
+    avoidable_folders = ['Inbox', 'Outbox',
+                         'Drafts', '[Gmail]', 'RSS Feeds', '']
     try:
         outlook = client.Dispatch("Outlook.Application")
         print("Called client dispatch")
 
         mapi = outlook.GetNamespace("MAPI")
         print("Got the Mapi object")
-    except: 
+    except:  # pylint: disable=W0702
         sys.exit("Mapi Object could not be created!")
-
-    # Iterate through all accounts 
+    # Iterate through all accounts
     for account in mapi.Accounts:
-
         email = account.DeliveryStore.DisplayName
         sender_name = email.split("@")[0]
-        
+
         all_folders = mapi.Folders(email).Folders
 
-        
         for each_folder in all_folders:
-                total_messages = 0
-                total_attachments = 0
+            total_messages = 0
+            total_attachments = 0
 
-                if each_folder.name == 'Inbox' or each_folder.name == 'Outbox' or each_folder.name == 'Drafts' or each_folder.name == '[Gmail]' or each_folder.name == 'RSS Feeds' or ":" in each_folder.name or "calendar" in each_folder.name.lower() or "this computer only" in each_folder.name.lower():
-                    continue 
-                else:
-                    path_original_name = f"{path_name}/{sender_name}/{date_today}/{each_folder}"
-                    messages = each_folder.Items
-                    print("Got all the messages!")
-                    
-                try:
-                    for message in list(messages):
-                        total_messages+=1
-                        print("Checking read/unread status")
-                        if status.lower() == "read": 
-                            if message.UnRead == True: 
-                                continue 
-                            else:
-                                pass 
-                        elif status.lower() == 'unread': 
-                            if message.UnRead == True: 
-                                pass 
-                            else: 
-                                continue 
-                        else: 
+            if (each_folder.name in avoidable_folders or
+                ":" in each_folder.name or
+                "calendar" in each_folder.name.lower() or
+                    "this computer only" in each_folder.name.lower()):
+                continue
+
+            path_original_name = f"{path_name}/{sender_name}/{date_today}/{each_folder}"
+            messages = each_folder.Items
+            print("Got all the messages!")
+
+            try:
+                for message in list(messages):
+                    total_messages += 1
+                    print("Checking read/unread status")
+                    if status.lower() == "read":
+                        if message.UnRead is True:
+                            continue
+
+                    elif status.lower() == 'unread':
+                        if message.UnRead is True:
                             pass
-                        total_messages +=1
-                        
+                        else:
+                            continue
+                    else:
+                        pass
+                    total_messages += 1
 
-                        print("Downloading Attachaments...")
-                        try:
-                            s = message.sender
-                            isAttachmentExist = False
-                            for idx,attachment in enumerate(message.Attachments):
-                                isAttachmentExist = True
-                                if idx==0: 
-                                    create_folder(path_original_name)
-                                    print(f"Folder created for account {sender_name}")
-                                filename_new = check_similar_file(attachment.FileName,path_original_name)
-                                print(f"Previous File name: ", attachment.FileName)
-                                print(f"New Filename: ", filename_new)
-                                attachment.SaveASFile(os.path.join(path_original_name, filename_new))
-                                print(f"attachment {attachment.FileName} from {s} saved")
-                                total_attachments +=1
-                            if isAttachmentExist:
-                                move_message(all_folders,date_and_time,message)
-                                                
+                    print("Downloading Attachaments...")
+                    try:
 
-                        except Exception as e:
-                            print("Error when saving the attachment:" + str(e))
-                            print(path_original_name)
-                    
-                except Exception as e:
-                    print("Error when processing emails messages:" + str(e))
-                MAIN_LIST.append([date_and_time,each_folder.name,total_messages,total_attachments])
-                # print([date_and_time,each_folder.name,total_messages,total_attachments])
-                df_to_excel_main_list(MAIN_LIST,date_and_time)
-                # save_csv([each_folder.name,total_messages,total_attachments],date_and_time)
+                        is_attachment_exist = False
+                        for idx, attachment in enumerate(message.Attachments):
+                            is_attachment_exist = True
+                            if idx == 0:
+                                create_folder(path_original_name)
+                                print(
+                                    f"Folder created for account {sender_name}")
+                            filename_new = check_similar_file(
+                                attachment.FileName, path_original_name)
+                            print("Previous File name: ", attachment.FileName)
+                            print("New Filename: ", filename_new)
+                            attachment.SaveASFile(os.path.join(
+                                path_original_name, filename_new))
+                            print(
+                                f"attachment {attachment.FileName} from {message.sender} saved")
+                            total_attachments += 1
+                        if is_attachment_exist:
+                            move_message(all_folders, date_and_time, message)
 
-    return total_messages,total_attachments       
+                    except Exception as exception_error:  # pylint: disable=W0703
+                        print("Error when saving the attachment:" +
+                              str(exception_error))
+                        print(path_original_name)
+
+            except Exception as exception_error:  # pylint: disable=W0703
+                print("Error when processing emails messages:" +
+                      str(exception_error))
+            MAIN_LIST.append([date_and_time, each_folder.name,
+                             total_messages, total_attachments])
+
+            df_to_excel_main_list(MAIN_LIST, date_and_time)
+
+    return total_messages, total_attachments
 
 
-
-def main(): 
+def main():
+    """Main Function"""
 
     path_name = input("Please enter download folder path: ")
-    status_code = input("Please choose\n1 for Read Emails\2 for Unread Emails\n3 for Read and Unread all Emails\n> ")
-    if status_code == "1": 
+    status_code = input(
+        "Please choose\n1 for Read Emails\2 for Unread Emails\n3 for Read and Unread all Emails\n> "
+    )
+    if status_code == "1":
         status = "read"
-    elif status_code == "2": 
+    elif status_code == "2":
         status = "unread"
-    else: 
+    else:
         status = "all"
-    date_time_today =  datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+    date_time_today = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
     date_today = f"{date_time_today}".split(" ")[0]
     path_name_modified = modify_path_name(path_name)
 
     try:
-        download_attachments(path_name_modified,date_today,status,date_time_today)
-    except Exception as e:
-        sys.exit(e) 
-main()
+        download_attachments(path_name_modified, date_today,
+                             status, date_time_today)
+    except Exception as exception_message:  # pylint: disable=W0703
+        sys.exit(exception_message)
+
+
+if __name__ == '__main__':
+    main()
